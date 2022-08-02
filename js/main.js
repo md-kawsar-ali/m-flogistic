5bd563471ab0928e35d92e2a3daf0976163a5356
mdkawsarali <facebookerjordan@gmail.com>
2022-08-02 14:44:55 +0600

    Form validation

--- js/main.js
+++ js/main.js
@@ -21,10 +21,48 @@ jQuery(function () {
                 Contact Form
 ===========================================*/
 const sendMail = () => {
+    // Input Fields
     const getName = jQuery('#name').val();
     const getEmail = jQuery('#email').val();
     const getPhone = jQuery('#phone').val();
     const getMessage = jQuery('#message').val();
+
+    // Form Validation
+    if (!/^[a-zA-Z ]+$/.test(getName)) {
+        $('#name-error').text('Enter Valid Name!');
+        $('#name-error').css('display', 'inline-block');
+        return null;
+    } else {
+        $('#name-error').text('');
+        $('#name-error').css('display', 'none');
+    }
+
+    if (!/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(getPhone)) {
+        $('#phone-error').text('Enter Valid Phone Number!');
+        $('#phone-error').css('display', 'inline-block');
+        return null;
+    } else {
+        $('#phone-error').text('');
+        $('#phone-error').css('display', 'none');
+    }
+
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(getEmail)) {
+        $('#email-error').text('Enter Valid Email Address!');
+        $('#email-error').css('display', 'inline-block');
+        return null;
+    } else {
+        $('#email-error').text('');
+        $('#email-error').css('display', 'none');
+    }
+
+    if (!/^(?=[\S\s]{5,255}$)[\S\s]*/.test(getMessage)) {
+        $('#message-error').text('Message should be min 5 to max 255 characters!');
+        $('#message-error').css('display', 'inline-block');
+        return null;
+    } else {
+        $('#message-error').text('');
+        $('#message-error').css('display', 'none');
+    }
 
     if (getName !== '' && getEmail !== '' && getPhone !== '' && getMessage !== '') {
         jQuery('#emailSendResult').html('Loading...');
